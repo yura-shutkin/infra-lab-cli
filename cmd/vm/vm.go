cca80a0732c02041160a28d89930e1874ab7b1c6
Shutkin Yura <yshutkin@infra-lab.xyz>
2025-08-04 18:08:55 +0200

    Fix wrong link to subcommands in vm

--- cmd/vm/vm.go
+++ cmd/vm/vm.go
@@ -2,8 +2,7 @@ package vm
 
 import (
 	"github.com/spf13/cobra"
-	"infra-lab-cli/cmd/cluster/kind"
-	"infra-lab-cli/cmd/cluster/minikube"
+	"infra-lab-cli/cmd/vm/podman"
 )
 
 var RootCmd = &cobra.Command{
@@ -13,6 +12,5 @@ var RootCmd = &cobra.Command{
 }
 
 func init() {
-	RootCmd.AddCommand(minikube.RootCmd)
-	RootCmd.AddCommand(kind.RootCmd)
+	RootCmd.AddCommand(podman.RootCmd)
 }
