d889dca8161fd6ddab4d78273a2aa6ba4612682a
Yura Shutkin <yshutkin@infra-lab.xyz>
2025-08-21 23:22:20 +0200

    Redo config (#14)
    * Redo env vars binding and default value settings, also remove code that executes config load second time
    * Add recovery to the reflection function
    
    Now, the default value must be defined as a tag default in a config type. This makes a single source of truth and eases the process of config modification

--- config/config.go
+++ config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"fmt"
 	"os/user"
+	"reflect"
 	"strings"
 
 	"github.com/spf13/viper"
@@ -10,136 +11,60 @@ import (
 
 var GlobalConfig ILCConfig
 
-func bindEnvVars(bindings map[string]string) (err error) {
-	for key, envVar := range bindings {
-		err = viper.BindEnv(key, envVar)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func generateEnvBindings() map[string]string {
-	bindings := make(map[string]string)
-
-	// Top-level fields
-	bindings["project_dir"] = "ILC__PROJECT_DIR"
-	bindings["apps.config_path"] = "ILC__CONFIG_PATH"
-
-	// App binaries
-	apps := []string{
-		"docker",
-		"helm",
-		"kind",
-		"minikube",
-		"podman",
-		"skopeo",
-		"webhook",
-	}
-	for _, app := range apps {
-		key := fmt.Sprintf("apps.%s.binary", app)
-		envVar := fmt.Sprintf("ILC__%s__BINARY", strings.ToUpper(app))
-		bindings[key] = envVar
-	}
-
-	// docker
-	bindings["apps.docker.config_path"] = "ILC__DOCKER__CONFIG_PATH"
-
-	// webhook
-	bindings["apps.webhook.webhooks_path"] = "ILC__WEBHOOK__WEBHOOKS_PATH"
-	bindings["apps.webhook.listen_addr"] = "ILC__WEBHOOK__LISTEN_ADDR"
-	bindings["apps.webhook.listen_port"] = "ILC__WEBHOOK__LISTEN_PORT"
-	bindings["apps.webhook.secret"] = "ILC__WEBHOOK__SECRET"
-	bindings["apps.webhook.prefix"] = "ILC__WEBHOOK__PREFIX"
-
-	// minikube
-	bindings["apps.minikube.cluster_name"] = "ILC__MINIKUBE__CLUSTER_NAME"
-	bindings["apps.minikube.kube_version"] = "ILC__MINIKUBE__KUBE_VERSION"
-	bindings["apps.minikube.nodes_count"] = "ILC__MINIKUBE__NODES_COUNT"
-	bindings["apps.minikube.driver"] = "ILC__MINIKUBE__DRIVER"
-	bindings["apps.minikube.cni"] = "ILC__MINIKUBE__CNI"
-	bindings["apps.minikube.cidr"] = "ILC__MINIKUBE__CIDR"
-	bindings["apps.minikube.cpus"] = "ILC__MINIKUBE__CPUS"
-	bindings["apps.minikube.memory"] = "ILC__MINIKUBE__MEMORY"
-	bindings["apps.minikube.disk_size"] = "ILC__MINIKUBE__DISK_SIZE"
-
-	// kind
-	bindings["apps.kind.cluster_name"] = "ILC__KIND__CLUSTER_NAME"
-	bindings["apps.kind.config_path"] = "ILC__KIND__CONFIG_PATH"
-
-	// podman
-	bindings["apps.podman.machine_name"] = "ILC__PODMAN__MACHINE_NAME"
-	bindings["apps.podman.cpus"] = "ILC__PODMAN__CPUS"
-	bindings["apps.podman.memory"] = "ILC__PODMAN__MEMORY"
-	bindings["apps.podman.disk_size"] = "ILC__PODMAN__DISK_SIZE"
-
-	return bindings
-}
-
-// TODO: add test to be sure all the configs has default value or not if this is expected
-// TODO: Consider to make new configs values and bind to vars easier if possible. Right now 3 places to modify and easy to forget or miss
-
-func setDefaultValues() {
-	viper.SetDefault("version", "1")
-
-	apps := []string{
-		"docker",
-		"helm",
-		"kind",
-		"minikube",
-		"podman",
-		"skopeo",
-		"webhook",
-	}
-	for _, app := range apps {
-		viper.SetDefault(fmt.Sprintf("apps.%s.binary", app), app)
-	}
-
-	usr, _ := user.Current()
-	viper.SetDefault("apps.docker.config_path", usr.HomeDir+"/.docker")
-
-	viper.SetDefault("apps.webhook.webhooks_path", "./webhooks.yaml")
-	viper.SetDefault("apps.webhook.listen_addr", "127.0.0.1")
-	viper.SetDefault("apps.webhook.listen_port", 8080)
-	viper.SetDefault("apps.webhook.secret", "")
-	viper.SetDefault("apps.webhook.prefix", "hooks")
-
-	viper.SetDefault("apps.kind.cluster_name", "kind-local")
-	viper.SetDefault("apps.kind.config_path", "")
-
-	viper.SetDefault("apps.minikube.cluster_name", "mini-local")
-	viper.SetDefault("apps.minikube.kube_version", "v1.30.0")
-	viper.SetDefault("apps.minikube.nodes_count", 1)
-	viper.SetDefault("apps.minikube.driver", "podman")
-	viper.SetDefault("apps.minikube.cni", "auto")
-	viper.SetDefault("apps.minikube.cidr", "172.16.0.0/16")
-	viper.SetDefault("apps.minikube.cpus", "2")
-	viper.SetDefault("apps.minikube.memory", "2G")
-	viper.SetDefault("apps.minikube.disk_size", "15G")
-
-	viper.SetDefault("apps.podman.machine_name", "podman-machine-default")
-	viper.SetDefault("apps.podman.cpus", "2")
-	viper.SetDefault("apps.podman.memory", "2G")
-	viper.SetDefault("apps.podman.disk_size", "10G")
-}
-
 func GetConfig() *ILCConfig {
 	return &GlobalConfig
 }
 
+func setDefaultsAndBindEnvs(cfg any, path, envPrefix string) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Printf("Recovered from panic in setDefaultsAndBindEnvs: %v\n", r)
+		}
+	}()
+
+	cfgType := reflect.TypeOf(cfg)
+	cfgValue := reflect.ValueOf(cfg)
+
+	for i := 0; i < cfgType.NumField(); i++ {
+		field := cfgType.Field(i)
+		tag := field.Tag.Get("mapstructure")
+
+		if field.Type.Kind() == reflect.Struct {
+			var newPath string
+			if path == "" {
+				newPath = tag
+			} else {
+				newPath = path + "." + tag
+			}
+			setDefaultsAndBindEnvs(cfgValue.Field(i).Interface(), newPath, envPrefix)
+		} else {
+			var cfgPath string
+			defaultValue := field.Tag.Get("default")
+			if path == "" {
+				cfgPath = tag
+			} else {
+				cfgPath = path + "." + tag
+			}
+			envPath := strings.ToUpper(strings.ReplaceAll(cfgPath, ".", "__"))
+			err := viper.BindEnv(cfgPath, envPrefix+"_"+envPath)
+			if err != nil {
+				fmt.Printf("BindEnv error: %v\n", err)
+			}
+			viper.SetDefault(cfgPath, defaultValue)
+
+		}
+	}
+}
+
+// TODO: add test to be sure all the configs has default value or not if this is expected
+
 func LoadConfig() (err error) {
-	viper.SetEnvPrefix("ILC_")
+	envPrefix := "ILC_"
+	viper.SetEnvPrefix(envPrefix)
 	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "__"))
 	viper.AutomaticEnv()
 
-	setDefaultValues()
-	envBindings := generateEnvBindings()
-
-	err = bindEnvVars(envBindings)
-	if err != nil {
-		return err
-	}
+	setDefaultsAndBindEnvs(GlobalConfig, "", envPrefix)
 	usr, _ := user.Current()
 	configPath := fmt.Sprintf("%s/.infra-lab.yaml", usr.HomeDir)
 
--- main.go
+++ main.go
@@ -1,16 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"infra-lab-cli/cmd"
-	"infra-lab-cli/config"
 )
 
 func main() {
-	err := config.LoadConfig()
-	if err != nil {
-		fmt.Println(err)
-	}
-
 	cmd.Execute()
 }
